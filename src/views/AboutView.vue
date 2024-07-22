a25935b832f895fc27990b72e7285fe0a8125c09
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-07-22 08:54:17 -0500

    Create AboutView.vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <div class="bag__suitcase">
+      <!--
+        1.WAND
+        2.MAP
+        3.Giratiempo
+        4.
+      -->
+    </div>
+    <div class="bag__spells_book">
+      <!--
+        1.Axios (Atrapar cosas  y otros items)
+        2.Lumos (para ver de noche y cosas oscuar)
+        3. etc.
+        4.
+      -->
+    </div>
+    <div class="bag__parchment_bestiary">
+      <!--
+        1.Fenix
+        2.Hipogrifo
+        3.etc
+        4.
+      -->
+    </div>
+    <div class="bag__parchment_runes">
+       <!--
+        1.Runa XXXX : Letra => 
+        2.Runa XXXX : Letra => 
+        3.Runa XXXX : Letra => 
+        4.
+      -->
+    </div>
+  </div>
+</template>
