3186506603fb9743274559ccdf9e1a405ca46250
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-07-23 06:21:13 -0500

    Create locations.gmaps.ts

---
+++ src/helpers/locations.gmaps.ts
@@ -0,0 +1,38 @@
+export const markers = {
+    P1_Q1:{
+        title: 'Hogwarts', 
+        icon: '/img/markers/houses.png',
+        position: {
+            lng: -74.11889, lat: 4.72413
+        },
+        tolerance: 10,
+        visible: true,
+    },
+    P1_Q2:{
+        title: 'Forest Brujas',
+        icon: '/img/markers/forest_brujas.png',
+        position: {
+            lng: -74.119546, lat: 4.7252882
+        },
+        tolerance: 10,
+        visible: true,
+    },
+    P2_Q1:{
+        title: 'Daily Prophet', // Con esto desbloqueamos las notificaciones y el patronus
+        icon: '/img/markers/dp.png',
+        position: {
+            lng: -74.1252674912281, lat: 4.716563957295621
+        },
+        tolerance: 10,
+        visible: true,
+    },
+    P3_Q1:{
+        title: 'Quidditch', // 
+        icon: '/img/markers/qd.png',
+        position: {
+            lng: -74.08643071746839, lat: 4.6947909
+        },
+        tolerance: 10,
+        visible: true,
+    },
+  }
