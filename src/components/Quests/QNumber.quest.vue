b496570698cdfa4569b4abadfbffea51293cf6d2
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-07-27 13:32:02 -0500

    Create QNumber.quest.vue

---
+++ src/components/Quests/QNumber.quest.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="quest quest__text"></div>
+  </template>
+    <script>
+  import { defineComponent } from '@vue/composition-api'
+  import { ref, onMounted } from 'vue'
+  //import IntroTutorial from '../components/Tutorials/Intro.tutorial.vue'
+  export default defineComponent({
+    components: {
+      //IntroTutorial
+    },
+    setup() {
+      const step = ref(0)
+  
+      onMounted(() => {
+        console.log('Holi')
+      })
+  
+      return { step }
+    }
+  })
+  </script>
+    
