93d2c93c24f5f19ff54460ac54886fe94e788e68
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-07-31 09:09:51 -0500

    Create basic.card.vue

---
+++ src/components/basic.card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card basic_card">
+    <div class="card__picture">
+      <img :src="picture" />
+    </div>
+    <div class="card__text">
+      <p v-for="parragraph of parragraphs" v-bind:key="parragraph">
+        {{ parragraph }}
+      </p>
+    </div>
+  </div>
+  <br>
+  <br>
+</template>
+<script>
+export default {
+  name: 'BasicCard',
+  components: {},
+  props: ['picture', 'parragraphs']
+}
+</script>
+<style lang="scss">
+
+</style>
