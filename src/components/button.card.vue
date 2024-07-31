dd4e5e3eb38f39e5064549c376038570fb77ba83
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-07-31 09:09:48 -0500

    Create button.card.vue

---
+++ src/components/button.card.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card button_card">
+      <div class="card__picture">
+        <img :src="picture" />
+      </div>
+      <div class="card__text">
+        <p v-for="parragraph of parragraphs" v-bind:key="parragraph">
+          {{ parragraph }}
+        </p>
+        <div class="buttons">
+            <button v-for="button in actions" :key="button.id"
+                @click="$emit('onAction', button.id)"
+            >
+            {{ button.label  }}
+            </button>
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+  </template>
+  <script>
+  export default {
+    name: 'ButtonCard',
+    components: {},
+    props: ['picture', 'parragraphs', 'actions'],
+    data () {
+      return {}
+    }
+  }
+  </script>
+  <style scoped lang="scss">
+  .swiper-slide {
+    height: auto;
+  }
+  </style>
